f8a4dfa11e4bab6d3d75a59a5f63b9f2940cf8f3
Samantha DeSmul <samanthadesmul@yahoo.com>
2019-04-10 14:47:06 -0400

    hee prints when the cointer % 3 == 0

--- heehaw.js
+++ heehaw.js
@@ -3,9 +3,11 @@ var hee = "Hee!"
 var haw = "Haw!"
 var counter = 1;
 
-while (counter <= 100) {
+while (counter <= maxNum) {
     if (counter % 3 == 0 && counter % 5 == 0) {
         console.log(hee + haw);
+    }else if(counter % 3 == 0){
+        console.log(hee);
     }
     counter++;
 }
