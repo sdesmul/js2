b9c79cf6c8452bee08c1a633f427117ad35272bc
Samantha DeSmul <samanthadesmul@yahoo.com>
2019-04-10 14:44:54 -0400

    created the while loop, have hee haw printing when counter % 3 ==0 && counter % 5 ==0

--- heehaw.js
+++ heehaw.js
@@ -3,3 +3,9 @@ var hee = "Hee!"
 var haw = "Haw!"
 var counter = 1;
 
+while (counter <= 100) {
+    if (counter % 3 == 0 && counter % 5 == 0) {
+        console.log(hee + haw);
+    }
+    counter++;
+}
