35f9a8f12b64d8685c76fc955b1ee3dab2702e86
Samantha DeSmul <samanthadesmul@yahoo.com>
2019-04-17 13:50:54 -0400

    create heeHaw function. added valdation to test if the value passed is greater then 0 and is numeric, if it doesnt pass these tests an error message is sent

--- heehaw.js
+++ heehaw.js
@@ -1,17 +1,27 @@
-var maxNum = 100;
-var hee = "Hee!"
-var haw = "Haw!"
-var counter = 1;
 
-while (counter <= maxNum) {
-    if (counter % 3 == 0 && counter % 5 == 0) {
-        console.log(hee + haw);
-    }else if(counter % 3 == 0){
-        console.log(hee);
-    }else if(counter % 5 == 0){
-        console.log(haw);
+
+
+function heeHaw(maxNum){
+
+    var hee = "Hee!";
+    var haw = "Haw!";
+    var counter = 1;
+    if(maxNum <= 0){
+        console.log("Please enter a number greater then 0")
+    } else if(isNaN(maxNum)==true){
+        console.log("Please enter a number greater then 0")
     } else{
-        console.log(counter);
+        while (counter <= maxNum) {
+            if (counter % 3 == 0 && counter % 5 == 0) {
+                console.log(hee + haw);
+            }else if(counter % 3 == 0){
+                console.log(hee);
+            }else if(counter % 5 == 0){
+                console.log(haw);
+            } else{
+                console.log(counter);
+            }
+            counter++;
+        }
     }
-    counter++;
 }
